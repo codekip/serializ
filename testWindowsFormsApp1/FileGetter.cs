e8b35c83e218280a85c1b9d33c72c88d72f8afc6
Nick Sawe <skelmdev@gmail.com>
2017-03-28 15:58:48 +0100

    Add project files.

---
+++ testWindowsFormsApp1/FileGetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using testWindowsFormsApp1.Properties;
+
+namespace testWindowsFormsApp1
+{
+    public static class FileGetter
+    {
+        public static IEnumerable<FileInfo> FileInfoAllFiles()
+        {
+            return new DirectoryInfo(Settings.Default["TDBpath"].ToString()).EnumerateFiles("*.*", SearchOption.AllDirectories);
+        }
+
+
+        public static IEnumerable<FileInfo> FileinfoFilteredByExtension(IEnumerable<FileInfo> filelist, string filter)
+        {
+            return filelist.Where(f => f.Extension == filter).ToArray();
+
+        }
+
+        public static IEnumerable<FileInfo> FileinfoFilteredByName(IEnumerable<FileInfo> filelist, string filter)
+        {
+            return filelist.Where(f => f.FullName.ToUpperInvariant().Contains(filter.ToUpperInvariant())).ToArray();
+
+        }
+    }
+}
---
+++ testWindowsFormsApp1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace testWindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            var x = FileGetter.FileInfoAllFiles();
+
+            var t = new TheFiles();
+            foreach (var file in x)
+            {
+                FileInfoWrapper f = new FileInfoWrapper(file);
+                t.Add(f);
+            }
+
+
+            var stream = new FileStream("TDBlist.xml", FileMode.Create);
+            new XmlSerializer(typeof(FileInfoWrapper)).Serialize(stream, t);
+
+            foreach (var file in x)
+            {
+                Debug.WriteLine(file.FullName + ' ' + file.LastWriteTime);
+            }
+
+        }
+    }
+
+    public class TheFiles
+    {
+        List<FileInfoWrapper> filez;
+        public TheFiles()
+        {
+            filez = new List<FileInfoWrapper>();
+        }
+
+        public void Add(FileInfoWrapper m)
+        {
+            filez.Add(m);
+        }
+    }
+
+    public class FileInfoWrapper
+    {
+        private FileInfo _file;
+        public string Name { get { return _file.FullName; } }
+        public DateTime LastWrite { get { return _file.LastWriteTime; } }
+        public string Extension { get { return _file.Extension; } }
+
+        public FileInfoWrapper(FileInfo f)
+        {
+            _file = f;
+        }
+
+        public FileInfoWrapper()
+        {
+
+        }
+    }
+}
